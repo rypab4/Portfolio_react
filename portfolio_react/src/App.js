04ea1450ce67170e25e26c8ddac34c216255580d
pabfam <pabalate@Pabalates-MBP.attlocal.net>
2023-03-25 14:19:25 -0700

    added component files

--- portfolio_react/src/App.js
+++ portfolio_react/src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './index.css';
+import Home from "./components/Home"
+import About from "./components/About"
+import Project from "./components/Project"
+import Contact from "./components/Contact"
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <h1>This is app.js</h1>
     </div>
   );
 }
