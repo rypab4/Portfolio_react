77a3d814aa612b9301c92d55101be4aea24b6951
pabfam <pabalate@Pabalates-MBP.attlocal.net>
2023-03-25 19:25:52 -0700

    initial stages of workin on project cards

--- portfolio_react/src/routes/Project.js
+++ portfolio_react/src/routes/Project.js
@@ -2,12 +2,14 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
 import HeroImg2 from '../components/HeroImg2'
+import WorkCard from '../components/WorkCard'
 
 const Project = () => {
   return (
   <div>  
     <Navbar/>
     <HeroImg2 heading= "PROJECTS." text="Some of my most recent works"/>
+    <WorkCard/>
     <Footer/>
   </div>
   )
