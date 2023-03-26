eb3e51a03f86c1aa4e2645857a3977b08da69273
pabfam <pabalate@Pabalates-MBP.attlocal.net>
2023-03-25 23:09:53 -0700

    added work, work card data

--- portfolio_react/src/components/WorkCard.js
+++ portfolio_react/src/components/WorkCard.js
@@ -1,27 +1,24 @@
 import "./WorkCardStyles.css"
 
 import React from 'react'
-import pro1 from "../assets/social_media_api.png";
+
 import {NavLink} from "react-router-dom"
-const WorkCard = () => {
+
+const WorkCard = (props) => {
   return (
-    <div className="work-container">
-        <h1 className="project-heading">Projects</h1>
-        <div className="project-container">
+
             <div className="project-card">
-                <img src={pro1} alt="" />
-                <h2 className="project-title">Social Media API</h2>
+                <img src={props.imgsrc} alt="image" />
+                <h2 className="project-title">{props.title}</h2>
                 <div className="pro-details">
-                    <p>This is text</p>
+                    <p>{props.text}</p>
                     <div className="pro-btns">
-                        <NavLink to="url.com" className="btn">View</NavLink>
+                        <NavLink to={props.view} className="btn">View</NavLink>
                         <NavLink to="url.com" className="btn">Source</NavLink>
                     </div>
                 </div>
             </div>
-        </div>
-
-    </div>
+        
   )
 }
 
